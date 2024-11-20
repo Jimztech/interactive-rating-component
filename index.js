0fc8bc268bc26b08d56eddbd08ad70b2ef899ef8
jimztech <ibezimjames20@gmail.com>
2024-11-20 16:52:07 +0100

    updated-js content

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-main.innerHtml = "You selected <!-- Add rating here --> out of 5
+const submitBtn = document.getElementById("submit");
+const main = document.getElementById("main");
 
-    Thank you!
+function newContent() {
+    main.innerHTML = `
+        <div class="image-container">
+            <img 
+                src="/images/illustration-thank-you.svg"
+                alt="thank you image"
+                class="thank-you"
+            />
+        </div>
+        <div class="rate-info-container">
+            <span class="rate-info">You selected</span>
+        </div>
+        <h1 class="second-header">Thank you!</h1>
+        <p class="p-tag">
+            We appreciate you taking the time to give a rating.
+            If you ever need more support, don't hesitate 
+            to get in touch!
+        </p>
+    `
+}
 
-    We appreciate you taking the time to give a rating. If you ever need more support, 
-    don’t hesitate to get in touch!
-
-    <!-- Thank you state end -->"
+submitBtn.addEventListener('click', newContent);
